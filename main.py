50df746c2e4ca1286db76f18001f2692166f0881
Kamel Idir <33938365+ekamidi@users.noreply.github.com>
2023-02-21 22:23:04 +0000

    Created main.py for user's location and verify location APIs.

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/api/user-location')
+def get_user_location():
+   # Get the user's location data from am external API or a database using the query parameters
+  lat = request.args.get('lat')
+  lng = request.args.get('lng')
+  response = requests.get(f'https://maps.googleapis.com/maps/api/location?lat={lat}&lng={lng}')
+  data = response.json()
+
+  # Return the user's location data as a JSON response
+  return jsonify(data)
+
+@app.route('/api/verify-location')
+def verify_user_location(lat, lng, expected_location):
+  # Setup an API request to the Google Maps Geocoding API
+  url = 'https://maps.googleapis.com/maps/api/geocode/json'
+  params = {'latlng': f'{lat},{lng}', 'key': 'YOUR_API_KEY'}
+  response = requests.get(url, params=params)
+
+  # Parse the API response to extract the location data
+  data = response.json()
+  location = data['results'][0]['formated_address']
+
+  # verify the location by comparing with the expected location
+  if location == expected_location:
+    return True
+  else:
+    return False
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
